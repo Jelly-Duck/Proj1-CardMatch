b81d191f61854713113d4a094c557219079e9ab9
Felix <andrewchris99@yahoo.com>
2024-02-12 09:00:06 -0500

    Commit for project submission

---
+++ README.md
@@ -0,0 +1,44 @@
+# Memory Card Game
+
+This will be a basic memory card game. There will be even numbers of cards to match letters A-H.
+
+## Instructions
+
+Click on each card to flip. If two card are matched they will stay face up. If two cards are clicked that do not match, they will turn face down again. Once all cards are matched and face up, the board will reset and mix with an additional set of cards to match. 
+
+## Pictures
+
+![game sketch](thumbnail.jpg)
+
+![alt text](<Screenshot from 2024-02-12 08-08-34.png>)
+
+![alt text](<Screenshot from 2024-02-12 08-08-51.png>)
+
+![alt text](<Screenshot from 2024-02-12 08-09-03.png>)
+
+## Pseudo Code
+
+- Create a blank board
+- Create blank squares for each card to fill
+- Create random card generator 1-100 that will generate in pairs
+- Create random generator for placing cards
+- Function to flip cards on click
+- Function to flip cards back upside down
+- Function to keep cards face up upon match
+- Function to end game once cards are all face up and display win message
+- Play again function
+
+## Technologies Used
+
+- HTML
+- CSS 
+- JavaScript
+- VSCode
+
+## Getting Started
+
+Click the link below to start the game. Click on cards to match. 7 incorrect matches dictates a loss.
+
+## Next Steps
+
+I would like to create a visual animation for the cards flipping over, instead of just showing the letters. I would also like to create levels, where after each 'win', more cards are added to the board.
---
+++ main.js
@@ -0,0 +1,145 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const board = document.getElementById("game-board");
+  const winMessage = document.getElementById("win-message");
+  const instructions = document.getElementById("instructions");
+  const playAgainButton = document.getElementById("play-again");
+  const incorrectGuessesElement = document.getElementById("incorrect-guesses");
+
+  const cards = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  const shuffledCards = shuffleArray(cards.concat(cards));
+
+  createBoard();
+  let flipping = false;
+  let badMatchesCount = 0;
+  const maxBadMatches = 10;
+  let matchedPairsCount = 0;
+
+  function createBoard() {
+    for (let card of shuffledCards) {
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+      cardElement.dataset.value = card;
+      cardElement.innerText = " ";
+
+      cardElement.addEventListener("click", flipCard);
+
+      board.appendChild(cardElement);
+    }
+  }
+
+  let firstCard = null;
+  let secondCard = null;
+
+  function flipCard() {
+    if (flipping) return;
+    if (!firstCard) {
+      firstCard = this;
+      firstCard.innerText = firstCard.dataset.value;
+    } else {
+      secondCard = this;
+      secondCard.innerText = secondCard.dataset.value;
+      if (firstCard.dataset.value === secondCard.dataset.value) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        matchedPairsCount++;
+        setTimeout(() => {
+          resetCards();
+        }, 500);
+      } else {
+        setTimeout(() => {
+          if (firstCard) {
+            firstCard.innerText = "?";
+          }
+          if (secondCard) {
+            secondCard.innerText = "?";
+          }
+          resetCards();
+          badMatchesCount++;
+          updateIncorrectGuesses();
+          checkForLose();
+        }, 500);
+      }
+    }
+  }
+
+  function resetCards() {
+    firstCard = null;
+    secondCard = null;
+    checkForWin();
+  }
+
+  function updateIncorrectGuesses() {
+    incorrectGuessesElement.innerText = `Incorrect Guesses: ${badMatchesCount}`;
+  }
+
+  function displayInstructions(show) {
+    instructions.style.display = show ? "block" : "none";
+  }
+
+  displayInstructions(true);
+
+  function displayWinMessage() {
+    winMessage.style.display = "flex";
+    winMessage.style.color = "rgb(37, 37, 153)";
+    winMessage.style.fontSize = "50px";
+    winMessage.style.webkitTextStroke = "1px white";
+    winMessage.style.textAlign = "center";
+
+    playAgainButton.style.display = "block";
+    playAgainButton.addEventListener("click", playAgain);
+  }
+
+  function checkForWin() {
+    console.log("Checking for win...");
+    if (matchedPairsCount === cards.length) {
+      displayWinMessage();
+      displayInstructions(false);
+    }
+  }
+
+  function checkForLose() {
+    if (badMatchesCount >= maxBadMatches) {
+      loseGame();
+    }
+  }
+
+  function loseGame() {
+    instructions.innerHTML = "Sorry, you lost the game. Try again!";
+    instructions.style.display = "block";
+    playAgainButton.style.display = "block";
+    playAgainButton.addEventListener("click", playAgain);
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  function playAgain() {
+    winMessage.style.display = "none";
+    playAgainButton.style.display = "none";
+    board.innerHTML = "";
+    flipping = false;
+    matchedPairsCount = 0;
+
+    badMatchesCount = 0;
+    updateIncorrectGuesses();
+
+    const shuffledCards = shuffleArray(cards.concat(cards));
+    createBoard();
+
+    firstCard = null;
+    secondCard = null;
+
+    instructions.innerHTML =
+      "~ Click any card to flip over. Match all the cards to win. ~";
+    instructions.style.display = "block";
+
+    displayInstructions(true);
+  }
+});
